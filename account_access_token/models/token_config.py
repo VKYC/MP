2aafc12083dbd013fa30e49be9df3b9072ea178c
AdrianRHV <rascuas_29998@hotmail.com>
2024-06-20 00:07:52 -0600

    [CHG]se agrega nueva logica para el correo.

--- account_access_token/models/token_config.py
+++ account_access_token/models/token_config.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 from odoo.exceptions import ValidationError, UserError
 import random
 import string
+import logging
+
+# _logger = logging.getLogger(__name__) solo debug
 
 class TokenConfig(models.Model):
     _name = 'token.config'
@@ -34,7 +37,9 @@ class TokenConfig(models.Model):
         if active_token:
             raise UserError("Ya existe un token activo. No se puede generar un nuevo token hasta que expire el actual.")
         vals['token'] = self._generate_token(vals.get('token_size', 6))
-        return super(TokenConfig, self).create(vals)
+        new_record = super(TokenConfig, self).create(vals)
+        self._send_notification_and_email(new_record)
+        return new_record
 
     @api.depends('token_size', 'token_duration')
     def _compute_token(self):
@@ -58,3 +63,37 @@ class TokenConfig(models.Model):
         """Genera un token aleatorio de tamaño especificado."""
         chars = string.ascii_uppercase + string.digits + string.ascii_lowercase
         return ''.join(random.choice(chars) for _ in range(size))
+
+    def _send_notification_and_email(self, record):
+        """Envía notificaciones y correos electrónicos a las personas adecuadas."""
+        job_titles = ['Gerente de administracion y finanzas', 'subgerente de administracion y finanzas']
+        employees = self.env['hr.employee'].search([
+            '|',
+            ('job_id.name', 'ilike', job_titles[0]),
+            ('job_id.name', 'ilike', job_titles[1])
+        ])
+
+        # _logger.info(f"Found {len(employees)} employees matching the criteria")
+
+        for employee in employees:
+            # _logger.info(f"Processing employee {employee.name} with email {employee.work_email}")
+            if employee.work_email:
+                self._send_email(employee.work_email, record)
+
+    def _send_email(self, email, record):
+        """Envía un correo electrónico con la información del token."""
+        mail_values = {
+            'subject': f'Token Generado: {record.token}',
+            'body_html': f"""
+                <p>Se ha generado un nuevo token.</p>
+                <p><strong>Token:</strong> {record.token}</p>
+                <p><strong>Fecha de inicio:</strong> {record.token_start_date}</p>
+                <p><strong>Fecha de vencimiento:</strong> {record.token_end_date}</p>
+            """,
+            'email_from': self.env.user.email,
+            'email_to': email,
+        }
+        mail = self.env['mail.mail'].create(mail_values)
+        # _logger.info(f"Mail created: {mail_values}")
+        mail.send()
+        # _logger.info(f"Mail sent to {email}")
